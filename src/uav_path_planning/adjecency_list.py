bdaefeb1de02266cadeecbc47135d0f11e7eed52
aisha <aicha.ouadah.usthb@gmail.com>
2022-07-19 16:40:23 +0100

    image to graph

---
+++ src/uav_path_planning/adjecency_list.py
@@ -0,0 +1,34 @@
+''' AdjecencyList is 
+    list of lists 
+    node : [connected nodes]
+    ...
+    node : [connected nodes]
+'''
+
+from itertools import cycle
+
+
+class AdjecencyList:
+
+    def __init__(self,nodes):
+        self.n=nodes
+        self.adjecencyList= [ ]* self.n
+
+    def print_adjecency_list(self):
+        self= cycle(self)
+        for i in range(self.n):
+            print("List of nodes of node {}",format(i),end="")
+            temp = self.adjecencyList[i]
+            while temp:
+                print("-> {} ",format(temp),end="")
+                temp=next(temp)
+            print("\n")
+
+    def add_node(self,connected_nodes):  
+        self.n = self.n + 1             
+        self.adjecencyList.append(connected_nodes)
+
+    
+
+
+
