55d006479241e2fb81bb61a76a6f5c8d3c16aa30
aisha <aicha.ouadah.usthb@gmail.com>
2022-09-02 22:20:36 +0100

    add sa tests

--- src/uav_path_planning/uav_path_planning_sa.py
+++ src/uav_path_planning/uav_path_planning_sa.py
@@ -1,6 +1,6 @@
 #NOTE 
 """
-
+SA Algorithm: 
 Initialisation 
 Trouver une solution initiale x ( aléatoirement ou avec une heuristique) 
 Choisir in température initiale TO 
@@ -24,7 +24,6 @@ setting can have a significant impact on the SA's performance."""
 
 # Imports 
 import math
-import numpy as np  
 import random as rd 
 import utils as ut
 import cv2 as cv
@@ -69,7 +68,7 @@ def findNeighbors(matrix,node):
     return neighbors
 
 
-def generatePath(matrix,start,end):  #FIXME  are you sure about the generated path 😭 
+def generatePath(matrix,start,end): 
     path=[]
     visited_nodes=[]
     path.append(start)
@@ -99,7 +98,6 @@ matrix=ut.imageToMatrix(img_path)
 
 def generateNeighboringPath(matrix,path): 
     x=rd.randint(0,len(path)-2)
-    print(x)
     neighbors=findNeighbors(matrix,path[x])
     output=path.copy()
     output[x]=neighbors[rd.randint(0,len(neighbors)-2)] 
@@ -126,29 +124,29 @@ def cost(path):
 
 # Initializing parameters
 
-IT = 10   # Initial temperature
-FT = 5   # Final temperature 
-N= 10 # Number of iterations
-NT= 5 # Number of iterations per Temperature 
-alpha = 0.1
-# Geometric Coefficient alpha
-# Transition Probability p
+IT = 5      # Initial temperature
+FT = 0.1    # Final temperature 
+N= 20       # Number of iterations
+NT= 15      # Number of iterations per Temperature 
+alpha = 0.1 # Geometric Coefficient alpha
 
-def SA(matrix,start,end):  
+
+def sa(matrix,start,end):  
     CT = IT   # Current temperature
     CP = generatePath(matrix,start,end) # current path (random)
-    CL = cost(CP) 
+    CL = len(CP) 
     OP = CP # optimal path
     OL = CL
     for i in range(N):
+        print("i= ",i)
         for j in range(NT):
             RP = generateNeighboringPath(matrix,CP)
-            RL = cost(RP)
+            RL = len(RP)
             if RL < CL:
                 CP = RP
                 CL = RL 
             else: 
-                p = math.exp(-(RL-CL)/CT)
+                p = math.exp(-(RL-CL)/CT) # Transition Probability 
                 r=rd.randint(0,1)
                 if r<p:
                     CP = RP
@@ -162,8 +160,24 @@ def SA(matrix,start,end):
     return OP , OL 
 
 
-start=(10,10)
+start=(10,9)
 end=(14,40)
-print(SA(matrix,start,end))
-    
-    
+#print(sa(matrix,start,end)) 
+# Save image
+
+def SA(img_path,start,end,image_name):
+    image=ut.imageToMatrix(img_path)
+    op , ol = sa(image,start,end) 
+    print("optimal path " , op , "path length " , ol)     
+    #Draw path in the image and save it
+    image=cv.imread(img_path)
+    new_image = ut.draw_path(img_path,op) 
+    saving_path = '/home/aisha/PFE/implementations/UAV-Path-Planning/src/tests/output/output_sa'
+    cv.imwrite(os.path.join(saving_path ,image_name), new_image)
+    return op, ol 
+
+#TEST
+
+img_path="/home/aisha/PFE/implementations/UAV-Path-Planning/data/room-png/64room_000.png"
+image_name = "64room_000.png"
+print(SA(img_path,start,end,"new_"+image_name))    
